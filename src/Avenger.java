0a6980eb7c21c42c2ff2ca0ce61802f3b649396d
jgere972 <jgere972@mtroyal.ca>
2023-09-28 20:48:07 -0600

    Second Commit: Comparable/Comparator done

--- src/Avenger.java
+++ src/Avenger.java
@@ -1,14 +1,14 @@
 
-public class Avenger implements Comparable<Avenger>{
-	
+public class Avenger implements Comparable<Avenger> {
+
 	private String name;
 	private String alias;
 	private String actor;
 	private int nameCount;
 	private int aliasCount;
 	private int actorCount;
-	
-	//Constructor
+
+	// Constructor
 	public Avenger() {
 		name = "";
 		alias = "";
@@ -17,24 +17,64 @@ public class Avenger implements Comparable<Avenger>{
 		aliasCount = 0;
 		actorCount = 0;
 	}
-	
-	//Getters and setters
-	public void setName(String n) {name = n;}
-	public void setAlias (String a) {alias = a;}
-	public void setActor (String a) {actor = a;}
-	public String getName() {return name;}
-	public String getAlias() {return alias;}
-	public String getActor() {return actor;}
-	public int getNameCount() {return nameCount;}
-	public int getAliasCount() {return aliasCount;}
-	public int getActorCount() {return actorCount;}
-	
+
+	// Getters and setters
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setAlias(String a) {
+		alias = a;
+	}
+
+	public void setActor(String a) {
+		actor = a;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public String getActor() {
+		return actor;
+	}
+
+	public int getNameCount() {
+		return nameCount;
+	}
+
+	public int getAliasCount() {
+		return aliasCount;
+	}
+
+	public int getActorCount() {
+		return actorCount;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		return false;
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		Avenger av = (Avenger) obj;
+		if (this.actor.compareTo(av.getActor()) == 0 || this.alias.compareTo(av.getAlias()) == 0
+				|| this.name.compareTo(av.getName()) == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
+	@Override
 	public int compareTo(Avenger a) {
-	 return 0;
+		return 0;
 	}
-	
+
 }
--- src/Manager.java
+++ src/Manager.java
@@ -1,57 +1,93 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 public class Manager {
-private Scanner input;
-private int totalwordcount = 0;
-private int topN = 4;
-private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
-public String[][] avengerRoster = { { "captainamerica", "rogers", "evans" }, { "ironman", "stark", "downey" },
-		{ "blackwidow", "romanoff", "johansson" }, { "hulk", "banner", "ruffalo" },
-		{ "blackpanther", "tchalla", "boseman" }, { "thor", "odinson", "hemsworth" },
-		{ "hawkeye", "barton", "renner" }, { "warmachine", "rhodes", "cheadle" },
-		{ "spiderman", "parker", "holland" }, { "wintersoldier", "barnes", "stan" } };
-	
+	private String file;
+	private Scanner input;
+	private int totalwordcount = 0;
+	private int topN = 4;
+	private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
+	public String[][] avengerRoster = { { "captainamerica", "rogers", "evans" }, { "ironman", "stark", "downey" },
+			{ "blackwidow", "romanoff", "johansson" }, { "hulk", "banner", "ruffalo" },
+			{ "blackpanther", "tchalla", "boseman" }, { "thor", "odinson", "hemsworth" },
+			{ "hawkeye", "barton", "renner" }, { "warmachine", "rhodes", "cheadle" },
+			{ "spiderman", "parker", "holland" }, { "wintersoldier", "barnes", "stan" } };
+
 	public static void main(String[] args) {
 		Manager manager = new Manager();
 		manager.run();
-	}	
-	
+	}
+
 	public void run() {
 		readInput();
 		printResults();
-		}
-	
+	}
+
 	public void readInput() {
-		input = new Scanner(System.in);
+		file = "input";
 		int wordCount = 0;
-		while (input.hasNext()) {
+		try {
+			File f = new File(file);
+			input = new Scanner(f);
+			while (input.hasNext()) {
 				String word = input.next();
-				word = word.trim().toLowerCase().split("'")[0].replaceAll("[^a-zA-Z\\s]", "");
-				if (word.length() != 0) wordCount ++;
+				word = word.trim().toLowerCase().split("'")[0].replaceAll("[^\\\\sa-zA-Z0-9]", "");
+				if (word.length() != 0)
+					wordCount++;
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not Found: " + e.getLocalizedMessage());
+			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		Comparator<Avenger> comp = new Comparator<Avenger>() {
+
+		Collections.sort(null, new Comparator<Avenger>() {
 			@Override
 			public int compare(Avenger a, Avenger b) {
-				int totalCount1 = a.getActorCount() + a.getAliasCount() + a.getNameCount();
-				int totalCount2 = b.getActorCount() + b.getAliasCount() + a.getNameCount();
-				if(totalCount1 == totalCount2) {
-					return a.getActorCount() - b.getActorCount();
-				}else {return totalCount1;}
+				int totCountDiff = (b.getActorCount() + b.getAliasCount() + a.getNameCount())
+						- (a.getActorCount() + a.getAliasCount() + a.getNameCount());
+				int nameDiff = a.getName().compareTo(b.getName());
+
+				if (totCountDiff == 0) {
+					return nameDiff;
+				} else {
+					return totCountDiff;
+				}
 			}
-		};
+		});
+
+		Collections.sort(null, new Comparator<Avenger>() {
+			@Override
+			public int compare(Avenger a, Avenger b) {
+				int actCountdiff = b.getActorCount() - a.getActorCount();
+				int lengDiff = a.getName().length() - b.getName().length();
+				int aliasDiff = a.getAlias().compareTo(b.getAlias());
+
+				if (actCountdiff == 0) {
+					if (lengDiff == 0) {
+						return aliasDiff;
+					} else {
+						return lengDiff;
+					}
+				} else {
+					return actCountdiff;
+				}
+			}
+		});
+
 		
 	}
+
 	public void printResults() {
 		/*
-		 * Make sure your format matches the sample output exactly. 
-		 * No extra empty lines, or text. Use the diff command to check if your output matches the sample outputs.
+		 * Make sure your format matches the sample output exactly. No extra empty
+		 * lines, or text. Use the diff command to check if your output matches the
+		 * sample outputs.
 		 */
 		System.out.println("Total number of words: " + totalwordcount);
 		System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
@@ -65,14 +101,16 @@ public String[][] avengerRoster = { { "captainamerica", "rogers", "evans" }, { "
 
 		// Print all ordered by AvengerComparatorFreqDesc
 		System.out.println("Top " + topN + " most popular avengers:");
-		// Todo: Print the topN most popular avengers, see the instructions for tie breaking
+		// Todo: Print the topN most popular avengers, see the instructions for tie
+		// breaking
 		// Make sure you follow the formatting example in the sample output
 		System.out.println();
 
 		// Print top n ordered by AvengerPerformerComparatorFreqDesc
 		System.out.println("Top " + topN + " most popular performers:");
-		// Todo: Print the topN most popular performers, see the instructions for tie breaking
-		// Make sure you follow the formatting example in the sample output	
+		// Todo: Print the topN most popular performers, see the instructions for tie
+		// breaking
+		// Make sure you follow the formatting example in the sample output
 		System.out.println();
 
 		// Print all ordered by alias alphabetically
@@ -81,5 +119,5 @@ public String[][] avengerRoster = { { "captainamerica", "rogers", "evans" }, { "
 		// Make sure you follow the formatting example in the sample output
 		System.out.println();
 	}
-	
+
 }
