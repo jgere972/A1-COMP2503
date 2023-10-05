0b943ec5a8e6b4c571aa57b615f24e0eda8110a1
jgere972 <jgere972@mtroyal.ca>
2023-10-05 11:41:22 -0600

    Commit 6: Almost there

--- src/A1.java
+++ src/A1.java
@@ -4,10 +4,10 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class A1 {
-	private String f;
 	private Scanner input;
 	private int totalwordcount = 0;
 	private int topN = 4;
@@ -41,138 +41,174 @@ public class A1 {
 	}
 
 	public void readInput() {
-		f = "input.txt";
-		totalwordCount = 0;
-		try {
-			File file = new File(f);
-			input = new Scanner(file);
-			while (input.hasNext()) {
-				String word = input.next();
-				word = word.trim().toLowerCase().split("'")[0].replaceAll("[^\\\\sa-zA-Z0-9]", "");
-				if (word.length() != 0) {
-					totalwordCount++;
-				}
-				// Searching through avengerRoater array and updating counters for the
-				// respective Avengers
-				for (int i = 0; i < avengerRoster.length; i++) {
-					for (int x = 0; x < avengerRoster[i].length; x++) {
-						// Captain America counter
-						if (word.equals(cptAmerica.getAlias())) {
-							cptAmerica.incrementAliasCount();
-							avengersList.add(cptAmerica);
-						} else if (word.equals(cptAmerica.getName())) {
-							cptAmerica.incrementNameCount();
-							avengersList.add(cptAmerica);
-						} else if (word.equals(cptAmerica.getActor())) {
-							cptAmerica.incrementActorCount();
-							avengersList.add(cptAmerica);
-						}
-						// Iron Man counter
-						else if (word.equals(ironMan.getAlias())) {
-							ironMan.incrementAliasCount();
-							avengersList.add(ironMan);
-						} else if (word.equals(ironMan.getName())) {
-							ironMan.incrementNameCount();
-							avengersList.add(ironMan);
-						} else if (word.equals(ironMan.getActor())) {
-							ironMan.incrementActorCount();
-							avengersList.add(ironMan);
-						}
-						// Black Widow counter
-						else if (word.equals(blackWidow.getAlias())) {
-							blackWidow.incrementAliasCount();
-							avengersList.add(blackWidow);
-						} else if (word.equals(blackWidow.getName())) {
-							blackWidow.incrementNameCount();
-							avengersList.add(blackWidow);
-						} else if (word.equals(blackWidow.getActor())) {
-							blackWidow.incrementActorCount();
-							avengersList.add(blackWidow);
-						}
-						// Hulk counter
-						else if (word.equals(hulk.getAlias())) {
-							hulk.incrementAliasCount();
-							avengersList.add(hulk);
-						} else if (word.equals(hulk.getName())) {
-							hulk.incrementNameCount();
-							avengersList.add(hulk);
-						} else if (word.equals(hulk.getActor())) {
-							hulk.incrementActorCount();
-							avengersList.add(hulk);
-						}
-						// Black Panther counter
-						else if (word.equals(blackPan.getAlias())) {
-							blackPan.incrementAliasCount();
-							avengersList.add(blackPan);
-						} else if (word.equals(blackPan.getName())) {
-							blackPan.incrementNameCount();
-							avengersList.add(blackPan);
-						} else if (word.equals(blackPan.getActor())) {
-							blackPan.incrementActorCount();
-							avengersList.add(blackPan);
-						}
-						// Thor counter
-						else if (word.equals(thor.getAlias())) {
-							thor.incrementAliasCount();
-							avengersList.add(thor);
-						} else if (word.equals(thor.getName())) {
-							thor.incrementNameCount();
-							avengersList.add(thor);
-						} else if (word.equals(thor.getActor())) {
-							thor.incrementActorCount();
-							avengersList.add(thor);
-						}
-						// Hawk Eye counter
-						else if (word.equals(hawkEye.getAlias())) {
-							hawkEye.incrementAliasCount();
-							avengersList.add(hawkEye);
-						} else if (word.equals(hawkEye.getName())) {
-							hawkEye.incrementNameCount();
-							avengersList.add(hawkEye);
-						} else if (word.equals(hawkEye.getActor())) {
-							hawkEye.incrementActorCount();
-							avengersList.add(hawkEye);
-						}
-						// War Machine counter
-						else if (word.equals(warMachine.getAlias())) {
-							warMachine.incrementAliasCount();
-							avengersList.add(warMachine);
-						} else if (word.equals(warMachine.getName())) {
-							warMachine.incrementNameCount();
-							avengersList.add(warMachine);
-						} else if (word.equals(warMachine.getActor())) {
-							warMachine.incrementActorCount();
-							avengersList.add(warMachine);
-						}
-						// Spider Man counter
-						else if (word.equals(spiderMan.getAlias())) {
-							spiderMan.incrementAliasCount();
-							avengersList.add(spiderMan);
-						} else if (word.equals(spiderMan.getName())) {
-							spiderMan.incrementNameCount();
-							avengersList.add(spiderMan);
-						} else if (word.equals(spiderMan.getActor())) {
-							spiderMan.incrementActorCount();
-							avengersList.add(spiderMan);
-						}
-						// Winter Soldier counter
-						else if (word.equals(winterSoldier.getAlias())) {
-							winterSoldier.incrementAliasCount();
-							avengersList.add(winterSoldier);
-						} else if (word.equals(winterSoldier.getName())) {
-							winterSoldier.incrementNameCount();
-							avengersList.add(winterSoldier);
-						} else if (word.equals(winterSoldier.getActor())) {
-							winterSoldier.incrementActorCount();
-							avengersList.add(winterSoldier);
-						}
-					}
-				}
+		input = new Scanner(System.in);
+		while (input.hasNext()) {
+			String word = input.next();
+			word = word.trim().toLowerCase().split("'")[0].replaceAll("[^\\\\sa-zA-Z0-9]", "");
+			if (word.length() != 0) {
+				totalwordCount++;
 			}
-			input.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not Found: " + e.getLocalizedMessage());
-			e.printStackTrace();
+			// Searching through avengerRoater array and updating counters for the
+			// respective Avengers
+
+			// Captain America counter
+			if (word.equals(cptAmerica.getAlias())) {
+				cptAmerica.incrementAliasCount();
+				checkList(cptAmerica);
+				continue;
+			} else if (word.equals(cptAmerica.getName())) {
+				cptAmerica.incrementNameCount();
+				checkList(cptAmerica);
+				continue;
+			} else if (word.equals(cptAmerica.getActor())) {
+				cptAmerica.incrementActorCount();
+				checkList(cptAmerica);
+				continue;
+			}
+
+			// Iron Man counter
+			if (word.equals(ironMan.getAlias())) {
+				ironMan.incrementAliasCount();
+				checkList(ironMan);
+				continue;
+			} else if (word.equals(ironMan.getName())) {
+				ironMan.incrementNameCount();
+				checkList(ironMan);
+				continue;
+			} else if (word.equals(ironMan.getActor())) {
+				ironMan.incrementActorCount();
+				checkList(ironMan);
+				continue;
+			}
+
+			// Black Widow counter
+			if (word.equals(blackWidow.getAlias())) {
+				blackWidow.incrementAliasCount();
+				checkList(blackWidow);
+				continue;
+			} else if (word.equals(blackWidow.getName())) {
+				blackWidow.incrementNameCount();
+				checkList(blackWidow);
+				continue;
+			} else if (word.equals(blackWidow.getActor())) {
+				blackWidow.incrementActorCount();
+				checkList(blackWidow);
+				continue;
+			}
+
+			// Hulk counter
+			if (word.equals(hulk.getAlias())) {
+				hulk.incrementAliasCount();
+				checkList(hulk);
+				continue;
+			} else if (word.equals(hulk.getName())) {
+				hulk.incrementNameCount();
+				checkList(hulk);
+				continue;
+			} else if (word.equals(hulk.getActor())) {
+				hulk.incrementActorCount();
+				checkList(hulk);
+				continue;
+			}
+
+			// Black Panther counter
+			if (word.equals(blackPan.getAlias())) {
+				blackPan.incrementAliasCount();
+				checkList(blackPan);
+				continue;
+			} else if (word.equals(blackPan.getName())) {
+				blackPan.incrementNameCount();
+				checkList(blackPan);
+				continue;
+			} else if (word.equals(blackPan.getActor())) {
+				blackPan.incrementActorCount();
+				checkList(blackPan);
+				continue;
+			}
+
+			// Thor counter
+			if (word.equals(thor.getAlias())) {
+				thor.incrementAliasCount();
+				checkList(thor);
+				continue;
+			} else if (word.equals(thor.getName())) {
+				thor.incrementNameCount();
+				checkList(thor);
+				continue;
+			} else if (word.equals(thor.getActor())) {
+				thor.incrementActorCount();
+				checkList(thor);
+				continue;
+			}
+
+			// Hawk Eye counter
+			if (word.equals(hawkEye.getAlias())) {
+				hawkEye.incrementAliasCount();
+				checkList(hawkEye);
+				continue;
+			} else if (word.equals(hawkEye.getName())) {
+				hawkEye.incrementNameCount();
+				checkList(hawkEye);
+				continue;
+			} else if (word.equals(hawkEye.getActor())) {
+				hawkEye.incrementActorCount();
+				checkList(hawkEye);
+				continue;
+			}
+
+			// War Machine counter
+			if (word.equals(warMachine.getAlias())) {
+				warMachine.incrementAliasCount();
+				checkList(warMachine);
+				continue;
+			} else if (word.equals(warMachine.getName())) {
+				warMachine.incrementNameCount();
+				checkList(warMachine);
+				continue;
+			} else if (word.equals(warMachine.getActor())) {
+				warMachine.incrementActorCount();
+				checkList(warMachine);
+				continue;
+			}
+
+			// Spider Man counter
+			if (word.equals(spiderMan.getAlias())) {
+				spiderMan.incrementAliasCount();
+				checkList(spiderMan);
+				continue;
+			} else if (word.equals(spiderMan.getName())) {
+				spiderMan.incrementNameCount();
+				checkList(spiderMan);
+				continue;
+			} else if (word.equals(spiderMan.getActor())) {
+				spiderMan.incrementActorCount();
+				checkList(spiderMan);
+				continue;
+			}
+
+			// Winter Soldier counter
+			if (word.equals(winterSoldier.getAlias())) {
+				winterSoldier.incrementAliasCount();
+				checkList(winterSoldier);
+				continue;
+			} else if (word.equals(winterSoldier.getName())) {
+				winterSoldier.incrementNameCount();
+				checkList(winterSoldier);
+				continue;
+			} else if (word.equals(winterSoldier.getActor())) {
+				winterSoldier.incrementActorCount();
+				checkList(winterSoldier);
+				continue;
+			}
+
+		}
+		input.close();
+	}
+	//Checks if an avenger mentioned is found in the avengersList
+	//If not add the avenger to the avengerList
+	public void checkList(Avenger a) {
+		if(!(avengersList.contains(a))){
+			avengersList.add(a);
 		}
 	}
 
@@ -182,69 +218,83 @@ public class A1 {
 		 * lines, or text. Use the diff command to check if your output matches the
 		 * sample outputs.
 		 */
-		System.out.println("Total number of words: " + totalwordcount);
-		System.out.println("Number of Avengers Mentioned: " + avengersList.size());
-		System.out.println();
-		
-		// Print all ordered by appearance
-		System.out.println("All avengers in the order they appeared in the input stream:");
-		// Todo: Print the list of avengers in the order they appeared in the input)
-		for(Avenger a: av) {
-			System.out.println(a.toString());
+		PrintStream fileStream;
+		try {
+			// Printing to File
+			fileStream = new PrintStream(new FileOutputStream(""));
+			System.setOut(fileStream);
+			// Output file content
+			System.out.println("Total number of words: " + totalwordcount);
+			System.out.println("Number of Avengers Mentioned: " + avengersList.size());
+			System.out.println();
+
+			// Print all ordered by appearance
+			System.out.println("All avengers in the order they appeared in the input stream:");
+			// Todo: Print the list of avengers in the order they appeared in the input)
+			for (Avenger a : av) {
+				System.out.println(a.toString());
 			}
-		
-		// Print all ordered by AvengerComparatorFreqDesc
-		System.out.println("Top " + topN + " most popular avengers:");
-		// Todo: Print the topN most popular avengers
-		Collections.sort(av, new Comparator<Avenger>() {
-			@Override
-			public int compare(Avenger a, Avenger b) {
-			int totCountDiff = (b.getActorCount() + b.getAliasCount() + a.getNameCount()) // Descending by total mentions
-			- (a.getActorCount() + a.getAliasCount() + a.getNameCount());
-			int nameDiff = a.getName().compareTo(b.getName()); // Ascending alphabetical order of Name
-			// Breaking Ties
-				if (totCountDiff == 0) {
-					return nameDiff;
-				} else {
-					return totCountDiff;
-				}
-			}
-					});
-		for(int i = 0; i < topN; i++) {
-			System.out.println(av.get(i).toString());
-			}
-		
-		// Print top n ordered by AvengerPerformerComparatorFreqDesc
-		System.out.println("Top " + topN + " most popular performers:");
-		// Todo: Print the topN most popular performers
-		Collections.sort(av, new Comparator<Avenger>() {
-			@Override
-			public int compare(Avenger a, Avenger b) {
-				int actCountdiff = b.getActorCount() - a.getActorCount(); // Descending Actor mentions
-				int lengDiff = a.getName().length() - b.getName().length(); // Ascending of name's length
-				int aliasDiff = a.getAlias().compareTo(b.getAlias()); // Ascending alphabetical order of Alias
-				// Breaking Ties
-				if (actCountdiff == 0) {
-					if (lengDiff == 0) {
-						return aliasDiff;
+			System.out.println();
+
+			// Print all ordered by AvengerComparatorFreqDesc
+			System.out.println("Top " + topN + " most popular avengers:");
+			// Todo: Print the topN most popular avengers
+			Collections.sort(av, new Comparator<Avenger>() {
+				@Override
+				public int compare(Avenger a, Avenger b) {
+					int totCountDiff = (b.getActorCount() + b.getAliasCount() + a.getNameCount()) // Descending by total
+																									// mentions
+							- (a.getActorCount() + a.getAliasCount() + a.getNameCount());
+					int nameDiff = a.getName().compareTo(b.getName()); // Ascending alphabetical order of Name
+					// Breaking Ties
+					if (totCountDiff == 0) {
+						return nameDiff;
 					} else {
-						return lengDiff;
+						return totCountDiff;
 					}
-				} else {
-					return actCountdiff;
 				}
+			});
+			for (int i = 0; i < topN; i++) {
+				System.out.println(av.get(i).toString());
 			}
-		});
-		for(int i = 0; i < topN; i++) {
-			System.out.println(av.get(i).toString());
+			System.out.println();
+
+			// Print top n ordered by AvengerPerformerComparatorFreqDesc
+			System.out.println("Top " + topN + " most popular performers:");
+			// Todo: Print the topN most popular performers
+			Collections.sort(av, new Comparator<Avenger>() {
+				@Override
+				public int compare(Avenger a, Avenger b) {
+					int actCountdiff = b.getActorCount() - a.getActorCount(); // Descending Actor mentions
+					int lengDiff = a.getName().length() - b.getName().length(); // Ascending of name's length
+					int aliasDiff = a.getAlias().compareTo(b.getAlias()); // Ascending alphabetical order of Alias
+					// Breaking Ties
+					if (actCountdiff == 0) {
+						if (lengDiff == 0) {
+							return aliasDiff;
+						} else {
+							return lengDiff;
+						}
+					} else {
+						return actCountdiff;
+					}
+				}
+			});
+			for (int i = 0; i < topN; i++) {
+				System.out.println(av.get(i).toString());
 			}
-		
-		// Print all ordered by alias alphabetically
-		System.out.println("All mentioned avengers in alphabetical order:");
-		// Todo: Print the list of avengers in alphabetical order
-		Collections.sort(av);
-		for(Avenger a: av) {
-		System.out.println(a.toString());
+			System.out.println();
+
+			// Print all ordered by alias alphabetically
+			System.out.println("All mentioned avengers in alphabetical order:");
+			// Todo: Print the list of avengers in alphabetical order
+			Collections.sort(av);
+			for (Avenger a : av) {
+				System.out.println(a.toString());
+			}
+			fileStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
 }
